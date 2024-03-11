17704710818b5cd4608b7f4888d9cf55148fd007
Namit Malasi <namitmalasi@gmail.com>
2024-03-11 20:49:19 +0530

    configured the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
