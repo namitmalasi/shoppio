3eaab91829bd22f416921fc10e7ed00ac1954cc0
Namit Malasi <namitmalasi@gmail.com>
2024-03-16 20:32:57 +0530

    created the products card on the homepage

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,12 +1,14 @@
 import Category from "../../components/category/Category";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Layout from "../../components/layout/Layout";
+import ProductsCard from "../../components/productsCard/ProductsCard";
 
 const Homepage = () => {
   return (
     <Layout>
       <HeroSection />
       <Category />
+      <ProductsCard />
     </Layout>
   );
 };
